69a7f2a9c48a0e157bb469acb9691c0b2a132a31
Andri Peti <apeti20@epoka.edu.al>
2026-01-02 15:27:24 +0100

    Add email/password authentication (phx.gen.auth)

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :incident_management_platform, :scopes,
+  user: [
+    default: true,
+    module: IncidentManagementPlatform.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: IncidentManagementPlatform.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :incident_management_platform,
   ecto_repos: [IncidentManagementPlatform.Repo],
   generators: [timestamp_type: :utc_datetime]
